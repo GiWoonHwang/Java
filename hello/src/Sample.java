0f1343767eea6892629a4b1dc254d4abe09b2dc9
JangDeukchun <rlaejrqo465@naver.com>
2023-02-19 13:46:29 +0900

    무슨 소리인지 모르겠음

--- hello/src/Sample.java
+++ hello/src/Sample.java
@@ -16,6 +16,65 @@ import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.ArrayList;
+
+// public class Sample extends Thread {
+//     public void run() { // Thread 를 상속하면 run 메서드를 구현해야 한다
+//         System.out.println("thread run");
+
+//     }
+
+//     public static void main(String[] args) {
+//         Sample sample = new Sample();
+//         sample.start();; // start() 로 쓰레드를 실행한다.
+//     }
+// }
+
+public class Sample implements Runnable {
+    int seq;
+
+    public Sample(int seq) {
+        this.seq = seq;
+    }
+
+    public void run() {
+        System.out.println(this.seq + "thread start"); // 쓰레드 시작
+        try{
+            Thread.sleep(1000);
+
+        } catch(Exception e){
+
+        }
+        System.out.println(this.seq + "thread end");
+    }
+
+    public static void main(String[] args){
+        ArrayList<Thread> threads = new ArrayList<>();
+        for(int i = 0; i < 10; i ++){ // 총 10개의 쓰레드를 생성하여 실행
+            Thread t = new Thread(new Sample(i));
+            t.start();
+            threads.add(t);
+        }
+
+        for(int i = 0; i<threads.size(); i++) {
+            Thread t = threads.get(i);
+            try{
+                t.join(); // t 쓰레드가 종료할 때까지 기다린다.
+            }catch(Exception e){
+
+            }
+
+            System.out.println("main end");
+        }
+
+
+
+    }
+
+}
+ 
+
+
 
 // public class Sample{
 //     public void shouldBeRun() {
