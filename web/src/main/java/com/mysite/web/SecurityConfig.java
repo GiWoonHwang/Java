4bdc5bb07cdcc59bba71b25d0ffe44b308153cb8
JangDeukChun <rlaejrqo465@naver.com>
2023-04-26 09:16:16 +0900

    스프링 시큐리티 보류

---
+++ web/src/main/java/com/mysite/web/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.mysite.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // ...
+                .authorizeRequests(authorize -> authorize
+                        .requestMatchers("/**").permitAll()
+
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().denyAll()
+                );
+        return http.build();
+    }
+
+
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests(
+//                authorize -> authorize
+//        ).antMatchers("/").permitAll();
+//        http
+//                .authorizeHttpRequests().requestMatchers(
+//                        new AntPathRequestMatcher("/**")).permitAll()
+//                .and()
+//                .csrf().ignoringRequestMatchers(
+//                        new AntPathRequestMatcher("/h2-console/**"))
+//                .and()
+//                .headers()
+//                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+//                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+//        ;
+//        return http.build();
+//    }
+}
+
+
+
