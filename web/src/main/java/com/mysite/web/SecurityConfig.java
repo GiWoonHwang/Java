2561c6b947a081b188bb8531f9aa6b0018956d75
JangDeukChun <rlaejrqo465@naver.com>
2023-04-26 10:32:30 +0900

    일단 회원가입 만듬

--- web/src/main/java/com/mysite/web/SecurityConfig.java
+++ web/src/main/java/com/mysite/web/SecurityConfig.java
@@ -3,31 +3,19 @@ package com.mysite.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                // ...
-                .authorizeRequests(authorize -> authorize
-                        .requestMatchers("/**").permitAll()
-
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().denyAll()
-                );
-        return http.build();
-    }
-
-
+//    @Bean
 //    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //        http.authorizeRequests(
 //                authorize -> authorize
@@ -45,7 +33,12 @@ public class SecurityConfig {
 //        ;
 //        return http.build();
 //    }
+//
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
-
-
-
+//
+//
+//
