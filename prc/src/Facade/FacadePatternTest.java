77493e7527817c441cdba1b7fca737add4e40a12
JangDeukChun <rlaejrqo465@naver.com>
2023-05-02 16:24:30 +0900

    복잡한 것들을 더 쉽게

---
+++ prc/src/Facade/FacadePatternTest.java
@@ -0,0 +1,15 @@
+package Facade;
+
+import java.sql.Connection;
+
+
+public class FacadePatternTest {
+
+    public static void main(String[] args) {
+        String tableName="Employee";
+
+        //generating MySql HTML report and Oracle PDF report using Facade
+        HelperFacade.generateReport(HelperFacade.DBTypes.MYSQL, HelperFacade.ReportTypes.HTML, tableName);
+        HelperFacade.generateReport(HelperFacade.DBTypes.ORACLE, HelperFacade.ReportTypes.PDF, tableName);
+    }
+}
---
+++ prc/src/Facade/MySqlHelper.java
@@ -0,0 +1,18 @@
+package Facade;
+
+import java.sql.Connection;
+
+public class MySqlHelper {
+    public static Connection getMySqlDBConnection(){
+        return null;
+    }
+
+    public void generateMySqlPDFReport(String tableName, Connection con){
+        // get data from table and generate pdf report
+    }
+
+    public void generateMySqlHTMLReport(String tableName, Connection con){
+        // get data from table and generate and report
+    }
+
+}
