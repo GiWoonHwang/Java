3ef108ebe1a04391e36593d6f403feb8c1da3a57
JangDeukChun <rlaejrqo465@naver.com>
2023-05-09 08:32:54 +0900

    객체지향은 왜 필요할까?

---
+++ oop/src/Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+    int result = 0;
+
+    int add(int num){
+        result += num;
+        return result;
+    }
+
+    int sub(int num){
+        result -= num;
+        return result;
+    }
+
+}
+
+// 계산기가 늘어날 때 마다 클래스를 추가할 것인가 ?
+//public class Calculator2 {
+//    static int result = 0;
+//
+//    static int add(int num){
+//        result += num;
+//        return result;
+//    }
+//}
---
+++ oop/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator cal1 = new Calculator();
+        Calculator cal2 = new Calculator();
+
+        System.out.println(cal1.add(3));
+        System.out.println(cal1.add(4));
+
+        System.out.println(cal2.add(3));
+        System.out.println(cal2.add(7));
+
+
+    }
+}
