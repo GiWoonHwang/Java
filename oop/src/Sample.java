10729ba81ce71eb73260d30720bb9ada491d772d
JangDeukChun <rlaejrqo465@naver.com>
2023-05-09 09:32:41 +0900

    잘 이해안됨?

--- oop/src/Sample.java
+++ oop/src/Sample.java
@@ -7,16 +7,42 @@ class Animal{
 
 }
 public class Sample {
+    int e;
+    int varTest(int d){ // 외부의 d 값을 증가시키는 방법
+        d++;
+        return d;
+    }
+
+    void sayNick(String nick){
+        if("fool".equals(nick)){
+            return ;
+        }
+        System.out.println("나의 별명은 "+nick+" 입니다." );
+    }
+    int sum(int a, int b){
+        return a + b;
+    }
     public static void main(String[] args){
-        Animal cat = new Animal();
-        cat.setName("boby");
+//    int a = 3;
+//    int b = 4;
+//
+    Sample sample = new Sample(); // 작성한 클래스를 단독으로 실행시켜 테스트할 때 자주 사용하는 방법이다.
+//    int c = sample.sum(a,b);s
+//    System.out.println(c); // 7
+//
+//    sample.sayNick("angel");
+//    sample.sayNick("fool");  // 출력되지 않는다.
 
-        Animal dog = new Animal();
-        dog.setName("happy");
+    int d = 1;
 
-        // 객체 변수는 공유하지 않는다
-        System.out.println(cat.name);
-        System.out.println(dog.name);
+    d = sample.varTest(d);
+    sample.e = 1;
+    sample.varTest(sample.e);
+    System.out.println(d); // 2
+    System.out.println(sample.e); // 2
+
+
+
 
 
 
