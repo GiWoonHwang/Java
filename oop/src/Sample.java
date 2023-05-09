4a5a224bf5df15fa01bd1d2b9101cbfd3ad12335
JangDeukChun <rlaejrqo465@naver.com>
2023-05-09 15:00:36 +0900

    하나안되면 다 안돼 -> main, 하나 안되도 다른건 되게 -> 메서드

--- oop/src/Sample.java
+++ oop/src/Sample.java
@@ -7,22 +7,21 @@ class FoolException extends Exception{
 }
 
 public class Sample {
-    public void sayNick(String nick){
-        // 컴파일 오류를 막기 위해 sayNick 메서드에서 try... catch 문으로 FoolException을 처리해야 한다.
-        try {
-            if ("fool".equals(nick)) {
-                throw new FoolException();
-            }
-            System.out.println("당신의 별명은 " + nick + " 입니다.");
-        }catch (FoolException e){
-            System.err.println("FoolException이 발생했습니다.");
+    public void sayNick(String nick) throws FoolException {
+        if("fool".equals(nick)) {
+            throw new FoolException();
         }
+        System.out.println("당신의 별명은 "+nick+" 입니다.");
     }
 
     public static void main(String[] args){
         Sample sample = new Sample();
-        sample.sayNick("fool");
-        sample.sayNick("genious");
+        try {
+            sample.sayNick("fool");
+            sample.sayNick("genious");
+        } catch (FoolException e) {
+            System.err.println("FoolException이 발생했습니다.");
+        }
 
     }
 }
