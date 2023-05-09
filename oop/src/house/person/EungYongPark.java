074678c31bedc41b52b6130553fd9b1aca3f9b8b
JangDeukChun <rlaejrqo465@naver.com>
2023-05-09 13:27:51 +0900

    protect는 상속받으면 가능

--- oop/src/house/person/EungYongPark.java
+++ oop/src/house/person/EungYongPark.java
@@ -1,4 +1,12 @@
-package house.person;
+package house.person; // HousePark 과 패키지가 서로 다르다.
 
-public class EungYongPark {
+import house.HousePark;
+
+public class EungYongPark extends HousePark {
+    public static void main(String[] args) {
+        EungYongPark eyp = new EungYongPark();
+        HousePark housePark = new HousePark();
+        System.out.println(eyp.lastname); // 상속한 클래스의 protected 변수는 접근이 가능하다.
+        System.out.println(housePark.info);
+    }
 }
