154ec3efa090242336085b8b534e760c808f049c
JangDeukChun <rlaejrqo465@naver.com>
2023-05-09 10:54:39 +0900

    인터페이스는 usb 포트이다

--- oop/src/Sample.java
+++ oop/src/Sample.java
@@ -1,3 +1,26 @@
+/*
+난 동물원의 사육사이다.
+육식동물이 들어오면 난 먹이를 던져준다.
+호랑이가 오면 사과를 던져준다.
+사자가 오면 바나나를 던져준다.
+ */
+
+interface Predator {
+    String getFood();
+
+    default void printFood() {
+        System.out.printf("my food is %s\n", getFood());
+    }
+
+    int LEG_COUNT = 4;  // 인터페이스 상수
+
+    static int speed() {
+        return LEG_COUNT * 30;
+    }
+
+
+}
+
 class Animal {
     String name;
 
@@ -6,52 +29,56 @@ class Animal {
     }
 }
 
-class Dog extends Animal { // Animal 클래스를 상속한다
-    Dog() { // 아무 내용이 없으면 디폴트 생성자라고 한다.
-
+class Tiger extends Animal implements Predator {
+    public String getFood() { // 인터페이스의 메서드는 항상 public으로 구현해야 한다.
+        return "apple";
     }
-    /*
-    * Dog 클래스에 name 이라는 객체변수와 setName 이라는 메서드를 만들지 않았지만 Animal 클래스를 상속했기 때문에 예제이서 보듯이 그대로 사용이 가능하다.
-    * Dog 클래스는 Animal 클래스를 상속했다. 즉, Dog는 Animal의 하위 개념이라고 할 수 있다. 이런 경우 Dog는 Animal에 포함되기 때문에 "개는 동물이다"라고 표현할 수 있다. 자바는 이러한 관계를 IS-A 관계라고 표현한다.
-    * */
-    void sleep() {
-        System.out.println(this.name +"   zzzz");
+}
+
+class Lion extends Animal implements  Predator {
+    public String getFood() {
+        return "banana";
     }
 
 }
 
-class HouseDog extends Dog {
-    HouseDog(String name){ // name 이라는 객체변수에 값을 무조건 설정해야만 객체가 생성될 수 있도록 강제할 수 있는 방법 1. 클래스명과 메서드명이 동일하다 2. 리턴타입을 정의하지 않는다ㄴ
-        this.name = name;
+class ZooKeeper{
+    void feed(Predator predator){
+        System.out.println("feed "+ predator.getFood());
     }
 
-    // 생성자 오버로딩
-    HouseDog(int type){
-        if (type == 1){
-            this.setName("yorkshire");
-        }
-        else if (type == 2){
-            this.setName("bulldog");
-        }
-    }
+    void feed(Lion lion){
+        System.out.println("feed banana");
+     }
 
-    void sleep() {
-        System.out.println(this.name + " zzz in house"); // 부모클래스의 메서드를 자식클래스가 동일한 형태로 또다시 구현하는 행위를 메서드 오버라이딩
-    }
+     /*
+     동물원에 호랑이와 사자뿐이라면 ZooKeeper 클래스는 더 이상 할일이 없겠지만 악어, 표범 등이 계속 추가된다면 ZooKeeper는 육식동물이 추가될 때마다 매번 다음과 같은 feed 메서드를 추가해야 한다.
 
-    void sleep(int hour){
-        System.out.println(this.name+" zzz in house for " + hour + " hours"); // 오버로딩
+     public void feed(Crocodile crocodile) {
+         System.out.println("feed strawberry");
+     }
+
+    public void feed(Leopard leopard) {
+        System.out.println("feed orange");
     }
+     */
+
 }
+
+
 
 
 public class Sample {
     public static void main(String[] args){
-        HouseDog happy = new HouseDog("happy"); // 문자열로 객체 생성
-        HouseDog yorkshire = new HouseDog(1); // 숫자값으로 객체 생성
+        ZooKeeper zooKeeper = new ZooKeeper();
+        Tiger tiger = new Tiger();
+        Lion lion = new Lion();
 
-        System.out.println(happy.name);
-        System.out.println(yorkshire.name);
+        zooKeeper.feed(tiger);
+        zooKeeper.feed(lion);
+
+
+
 
     }
 
