9861843c0dbda9ce450924502b9c3ffe00b78b68
JangDeukChun <rlaejrqo465@naver.com>
2023-05-09 10:19:51 +0900

    상속

--- oop/src/Sample.java
+++ oop/src/Sample.java
@@ -1,27 +1,50 @@
-class Updater {
-    void update(Counter counter){ // 단순히 값을 받으면 객체 값을 변경할 수 없다
-        counter.count ++;
+class Animal {
+    String name;
+
+    void setName(String name){
+        this.name = name;
     }
 }
 
-class Counter {
-    int count = 0; // 객체변수
+class Dog extends Animal { // Animal 클래스를 상속한다
+    /*
+    * Dog 클래스에 name 이라는 객체변수와 setName 이라는 메서드를 만들지 않았지만 Animal 클래스를 상속했기 때문에 예제이서 보듯이 그대로 사용이 가능하다.
+    * Dog 클래스는 Animal 클래스를 상속했다. 즉, Dog는 Animal의 하위 개념이라고 할 수 있다. 이런 경우 Dog는 Animal에 포함되기 때문에 "개는 동물이다"라고 표현할 수 있다. 자바는 이러한 관계를 IS-A 관계라고 표현한다.
+    * */
+    void sleep() {
+        System.out.println(this.name +"   zzzz");
+    }
+
 }
 
+class HouseDog extends Dog {
+    // 집에서 키우는 개들은 집에서만 잠을 잔다고 한다
+    void sleep() {
+        System.out.println(this.name + " zzz in house"); // 부모클래스의 메서드를 자식클래스가 동일한 형태로 또다시 구현하는 행위를 메서드 오버라이딩
+    }
+
+    void sleep(int hour){
+        System.out.println(this.name+" zzz in house for " + hour + " hours");
+    }
+}
 
 
 public class Sample {
     public static void main(String[] args){
-        Counter myCounter = new Counter();
-        System.out.println("before update:"+myCounter.count);
+        Dog dog = new Dog();
+        dog.setName("poppy");
 
-        Updater myUpdater = new Updater();
-        myUpdater.update(myCounter); // 이렇게 메서드의 입력으로 객체를 전달받는 경우에는 메서드가 입력받은 객체를 그대로 사용하기 때문에 메서드가 객체의 속성값을 변경하면 메서드 수행 후에도 객체의 변경된 속성값이 유지된다.
-
-        System.out.println("after update:"+myCounter.count);
+        System.out.println(dog.name);
+        dog.sleep();
 
 
+        Animal giwoon = new Dog(); // Dog객체를 Animal 자료형으로 사용할 경우에는 Dog 클래스에만 존재하는 sleep 메서드를 사용할 수 없다는 점이다, 개로 만든 객체는 동물 자료형이다.
+//        Dog dog = new Animal(); // 컴파일 오류: 부모 클래스로 만든 객체는 자식 클래스의 자료형으로 사용할 수 없다.
 
+        HouseDog houseDog = new HouseDog();
+        houseDog.setName("happy");
+        houseDog.sleep();  // happy zzz 출력
+        houseDog.sleep(3);  // happy zzz in house for 3 hours 출력
     }
 
 
