0aba3bda57960d0173841b875a6d3c7fdb0b2604
JangDeukChun <rlaejrqo465@naver.com>
2023-05-10 14:28:02 +0900

    2-08 ROOT URL

--- sbb/src/main/java/com/mysite/sbb/MainController.java
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -9,9 +9,9 @@ public class MainController {
     @Controller인 경우에 바디를 자바객체로 받기 위해서는 @ResponseBody 어노테이션을 반드시 명시해주어야한다.
      @ResponseBody 애너테이션을 생략한다면 "index"라는 이름의 템플릿 파일을 찾게 된다.
      */
-    @GetMapping("/sbb")
-    @ResponseBody
-    public String index() {
-        return "안녕하세요 환영합니다.";
+    @GetMapping("/")
+    public String root() {
+
+        return "redirect:/question/list";
     }
 }
