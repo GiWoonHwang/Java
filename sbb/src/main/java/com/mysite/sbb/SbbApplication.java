65655fdc784982825cea9bfba4902a0b3f75b6c4
JangDeukChun <rlaejrqo465@naver.com>
2023-05-10 09:10:02 +0900

    인텔리제이 테스트 커밋

--- sbb/src/main/java/com/mysite/sbb/SbbApplication.java
+++ sbb/src/main/java/com/mysite/sbb/SbbApplication.java
@@ -3,7 +3,7 @@ package com.mysite.sbb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 애너테이션을 통해 모든 스트링부트의 설정이 관리된다.
 public class SbbApplication {
 
 	public static void main(String[] args) {
