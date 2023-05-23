2663b68d5fec28240bea86233f72e4e91b937096
JangDeukChun <rlaejrqo465@naver.com>
2023-05-23 09:43:13 +0900

    3-05 스프링 시큐리티

---
+++ sbb/src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.mysite.sbb;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity // 모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만드는 애너테이션이다. SpringSecurityFilterChain이 동작하여 URL 필터가 적용된다.
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().requestMatchers(
+                new AntPathRequestMatcher("/**")).permitAll()
+        ;
+        return http.build();
+    }
+}
