a5aa1c8a08ba630d03a32299b5b2141082fc9957
JangDeukChun <rlaejrqo465@naver.com>
2023-06-13 17:12:12 +0900

    yml을 객체로 맵핑하는 방법

--- dustin/src/main/java/io/dustin/apps/common/config/RigoWeb3Configuration.java
+++ dustin/src/main/java/io/dustin/apps/common/config/RigoWeb3Configuration.java
@@ -1,20 +1,22 @@
 package io.dustin.apps.common.config;
 
+import io.dustin.apps.common.model.RigoProperties;
 import io.rigo.sdk.common.client.RigoWeb3;
 import io.rigo.sdk.common.client.factory.RigoApiClientFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class RigoWeb3Configuration {
 
-    @Value("${rigo.node.url}")
-    private String baseUrl;
+    private final RigoProperties prop;
 
     @Bean
     public RigoWeb3 rigoClient() {
-        RigoApiClientFactory factory = RigoApiClientFactory.newInstance(baseUrl);
+        RigoApiClientFactory factory = RigoApiClientFactory.newInstance(prop.getNodeUrl());
         return factory.rigoWeb3();
     }
 }
