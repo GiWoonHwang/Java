9e992b0c1c128e03e89a0a35bbf498363cd2c299
JangDeukChun <rlaejrqo465@naver.com>
2023-09-20 14:50:32 +0900

    update

--- app/src/main/java/io/dustin/apps/SbbApplication.java
+++ app/src/main/java/io/dustin/apps/SbbApplication.java
@@ -20,18 +20,6 @@ public class SbbApplication {
 
 	public static void main(String[] args)  {
 		SpringApplication.run(SbbApplication.class, args);
-		/**
-		 *		구독기능 테스트
-		 * 		RigoWeb3Configuration test = new RigoWeb3Configuration();
-		 *
-		 * 		Subscriber subscriber = new Subscriber("ws://192.168.252.60:26657/websocket");
-		 * 		SubscriberCallback callback = (message) -> {
-		 * 			System.out.println(message);
-		 *                };
-		 * 		subscriber.start(DefaultEventType.NEW_BLOCK, callback);
-		 */
-
-
 
 	}
 
